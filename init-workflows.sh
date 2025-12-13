80f06f8132ce318dc9508c701337c3d3e2759ca5
Tomek Wenzel <tomek.wenzel@swisstph.ch>
2025-12-13 23:18:43 +0100

    fix: workflow import

--- init-workflows.sh
+++ init-workflows.sh
@@ -6,7 +6,7 @@ if [ -z "${GITHUB_URL}" ]; then
   exit 1
 fi
 
-work_dir="/home/node/.n8n"
+work_dir="/home/node/.n8n/n8nworkflows"
 workflow_dir="/home/node/.n8n/workflows"
 
 if [ -d "${work_dir}/.git" ]; then
@@ -15,6 +15,12 @@ else
   git clone "${GITHUB_URL}" "${work_dir}"
 fi
 
-n8n import:workflow --input="${work_dir}/workflow.json"
+mkdir -p "$workflow_dir"
+
+cp "$work_dir"/*.json "$workflow_dir"/
+
+for file in "$workflow_dir"/*.json; do
+    n8n import:workflow --input="$file" --overwrite
+done
 
 exec n8n
